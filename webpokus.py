b62ae822e3faa233700d35617cb50d87d77ad065
Dodga010 <dolezal45@gmail.com>
2025-02-20 21:26:53 +0100

    Update webpokus.py

--- webpokus.py
+++ webpokus.py
@@ -177,7 +177,7 @@ def main():
             numeric_cols = df.select_dtypes(include=['number']).columns
             st.dataframe(df.style.format({col: "{:.1f}" for col in numeric_cols}))
 
-  elif page == "Head-to-Head Comparison":
+elif page == "Head-to-Head Comparison":
         df = fetch_team_data()
         if df.empty:
             st.warning("No team data available.")
@@ -218,7 +218,7 @@ def main():
                 fig2 = px.bar(team2_stats, x="Stat", y="Value", title=f"{team2} Stats Per Game", color="Stat")
                 st.plotly_chart(fig2)
 
-    elif page == "Referee Stats":
+ elif page == "Referee Stats":
         df_referee = fetch_referee_data()
 
         if df_referee.empty:
@@ -235,7 +235,7 @@ def main():
                                  color="Referee")
             st.plotly_chart(fig_referee)
 
-    elif page == "Shot Chart":
+ elif page == "Shot Chart":
         st.subheader("🎯 Player Shot Chart")
         players = fetch_players()
         if not players:
